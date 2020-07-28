893e6546b5d7433ff55fb2baf8a5c7ccf90fb5e1
Esra Kabakbaş <esrakabakbas@users.noreply.github.com>
2020-07-28 17:43:52 +0300

    MongoDB Products App added

---
+++ Products App with MongoDB/public/controller/controller.js
@@ -0,0 +1,49 @@
+angular.module('productsApp', [])
+    .controller('AppController', function ($scope, $http) {
+        console.log("Hello from controller")
+        var currentId = 1;
+
+        var bring = function() {
+            $http.get('/products').then(function(response){
+            console.log("I got the data I requested")
+            $scope.products = response.data;
+        })
+    }
+    bring();
+
+    $scope.createProduct = function(){
+        
+        $scope.product.id = currentId;
+        $http.post("/products", $scope.product).then(function(response){
+            console.log(response)
+            currentId++;
+            bring();
+        })
+    }
+
+    $scope.remove = function(id){
+        console.log(id);
+        $http.delete("/products/"+id).then(function(response){
+            currentId--;
+            bring();
+        });
+    }
+
+    $scope.updateProduct = function(id){
+        console.log(id + " buraaaa");
+
+        $http.get('/products/' + id).then(function(response) {
+            $scope.product = response.data;
+            console.log(response.data.name)
+            console.log($scope.product.name + " name buraaaa");
+
+            $http.put("/products/"+$scope.product._id, $scope.product).then(function(response){
+                bring();
+            })
+          });
+        
+        
+    }
+
+
+    })
---
+++ Products App with MongoDB/server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('products', ['products']);
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.json())
+
+
+app.get('/products', function(req,res){
+    console.log("I got the cheriiff")
+
+    db.products.find(function(err,docs){
+        console.log(docs);
+        res.json(docs)
+    })
+})
+
+app.post('/products', function(req, res){
+    console.log(req.body)
+    db.products.insert(req.body, function(err,doc){
+        res.json(doc)
+    })
+})
+
+app.delete("/products/:id", function(req,res){
+    var id = req.params.id;
+    console.log(id)
+    db.products.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+})
+
+app.get("/products/:id", function(req,res){
+    var id = req.params.id;
+    console.log(id + " serverside id");
+    db.products.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+})
+
+app.put("/products/:id", function(req, res){
+    var id = req.params.id;
+    console.log("LOOOK : " + req.body.name)
+    db.products.findAndModify({
+        query: {_id: mongojs.ObjectId(id)},
+        update: {$set: {name: req.body.name}},
+        new: true}, function(err, doc) {
+            res.json(doc);
+        }
+    )
+})
+
+app.listen(3000);
+console.log("Server running on 3000")
